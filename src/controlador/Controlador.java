a8c39099c22ed3f74f505350fc5cbb71e39f14c9
Elcida <Elcida@192.168.2.9>
2011-07-24 20:25:20 -0430

    Se elimino la clase Circulo

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -6,9 +6,9 @@ import java.awt.event.MouseEvent;
 import java.util.ListIterator;
 
 import Vista.Vista;
-import modelo.Circulo;
 import modelo.Cuadrado;
 import modelo.Maquina;
+import modelo.Compilador;
 import modelo.Programa;
 import modelo.Figura;
 import modelo.Modelo;
@@ -60,7 +60,7 @@ public class Controlador {
 			this.anyadirFigura(new Programa(ev.getPoint(),50,40));			
 		}else if(SwingUtilities.isMiddleMouseButton(ev))//click boton medio añade figura tipo circulo
 		{
-			this.anyadirFigura(new Maquina(ev.getPoint(),40));
+			this.anyadirFigura(new Compilador(120, 40, 40, ev.getPoint()));
 		}
 		vista.repaint();		
 	}
@@ -83,90 +83,3 @@ public class Controlador {
 
 }
 
-=======
-package controlador;
-
-import java.awt.Point;
-import javax.swing.SwingUtilities;
-import java.awt.event.MouseEvent;
-import java.util.ListIterator;
-
-import Vista.Vista;
-import modelo.Circulo;
-import modelo.Compilador;
-import modelo.Cuadrado;
-import modelo.Maquina;
-import modelo.Figura;
-import modelo.Modelo;
-
-public class Controlador {
-	
-	private Modelo modelo;
-	private Vista vista;
-	private Figura seleccionada;
-	
-	public Controlador(Modelo modelo, Vista vista){
-		this.modelo=modelo;
-		this.vista=vista;
-		seleccionada=null;
-	}
-	
-	public Figura obtenerFigura(Point posicion){
-		ListIterator<Figura> it=modelo.getListado().listIterator();
-	    while (it.hasNext()) {
-	    	Figura tmp=it.next();
-	    		if(tmp.dentroFigura(posicion)){
-	    			tmp.setSeleccionada(true);
-	    			return tmp;
-	    		}
-		    }
-	    return null;
-	}
-
-	public void cambiarPosicion(Figura f, Point p){
-		f.setPosicion(p);
-	}
-	
-	public Vista getVista(){
-		return vista;
-	}
-	
-	public void anyadirFigura(Figura f){
-		modelo.anyadirFigura(f);
-	}
-	
-	public Figura getFiguraEn(Point p){
-		return modelo.getFiguraEn(p);
-	}
-	
-	public void eVmousePressed(MouseEvent ev) {
-		if(SwingUtilities.isLeftMouseButton(ev)){ 			//Click boton izquierdo selecciona figura
-			seleccionada=this.getFiguraEn(ev.getPoint());
-		}else if(SwingUtilities.isRightMouseButton(ev)){		//click boton izquierdo añade figura tipo cuadrado
-			this.anyadirFigura(new Compilador(120, 40, 40, ev.getPoint()));			
-		}else if(SwingUtilities.isMiddleMouseButton(ev))//click boton medio añade figura tipo circulo
-		{
-			this.anyadirFigura(new Circulo(ev.getPoint(),40));
-		}
-		vista.repaint();		
-	}
-	
-	public void eVmouseDragged(MouseEvent ev) {
-		if(seleccionada!=null){
-			//El movimiento puede ser mas fluido recalculando el pto
-			this.cambiarPosicion(seleccionada, ev.getPoint());
-			vista.repaint();
-		}
-	}
-
-	public void eVmouseReleased (MouseEvent ev) {
-		vista.repaint();
-		if(seleccionada!=null){
-			seleccionada.setSeleccionada(false);
-			seleccionada=null;
-		}
-	}
-
-}
-
->>>>>>> 333e31af56081c5b1f7949512fe38d3cf705f1bd
